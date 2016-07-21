66e5332ec20a8d4faf286b75d3e8367819b7f812
wispwisp <wisp@gcnet.ru>
2016-07-21 14:21:45 +0300

    some emacs notes

---
+++ emacs/for_gui.el
@@ -0,0 +1,4 @@
+(custom-set-variables
+ '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(custom-enabled-themes (quote (deeper-blue))))
+(custom-set-faces)
