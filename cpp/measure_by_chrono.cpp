a99875fcc9df305bb75f89d4df5f1955d63f5096
wisp <wisp@gcnet.ru>
2018-10-05 16:57:28 +0300

    chrono

--- cpp/measure_by_chrono.cpp
+++ cpp/measure_by_chrono.cpp
@@ -1,8 +1,9 @@
 auto t1 = std::chrono::steady_clock::now();
 
 // ...
+std::this_thread::sleep_for(std::chrono::seconds(5));
 
-using time = std::chrono::milliseconds;
-auto m = std::chrono::duration_cast<time>(std::chrono::steady_clock::now() - t1);
-std::cout << m.count() << "\n";
+// seconds/milliseconds(1000seconds)/...
+auto m = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - t1);
+std::cout << m.count() << " seconds\n";
 
